9e9b36c263829b9753f3b5fc39356b9d4bd25f27
shujinarazaki <shujinarazaki@protonmail.com>
2020-10-12 22:30:19 +0900

    Lecture 09 done.
    	modified:   EmojiArt.xcodeproj/project.pbxproj
    	modified:   EmojiArt.xcodeproj/project.xcworkspace/xcuserdata/nash.xcuserdatad/UserInterfaceState.xcuserstate
    	new file:   EmojiArt/EmojiArtDocumentPalette.swift
    	modified:   EmojiArt/Info.plist
    	new file:   EmojiArt/PaletteChooser.swift
    	new file:   EmojiArt/Spinning.swift

---
+++ EmojiArt/PaletteChooser.swift
@@ -0,0 +1,32 @@
+//
+//  PalletChooser.swift
+//  EmojiArt
+//
+//  Created by 楢崎修二 on 2020/10/12.
+//
+
+import SwiftUI
+
+struct PaletteChooser: View {
+    @ObservedObject var document: EmojiArtDocument
+    
+    @Binding var chosenPalette: String
+    
+    var body: some View {
+        HStack {
+            Stepper(onIncrement: {
+                self.chosenPalette = self.document.palette(after: self.chosenPalette)
+            }, onDecrement: {
+                self.chosenPalette = self.document.palette(before: self.chosenPalette)
+            }, label: { EmptyView() })
+            Text(self.document.paletteNames[self.chosenPalette] ?? "")
+        }
+        .fixedSize(horizontal: true, vertical: false)
+    }
+}
+
+struct PalletChooser_Previews: PreviewProvider {
+    static var previews: some View {
+        PaletteChooser(document: EmojiArtDocument(), chosenPalette: Binding.constant(""))
+    }
+}
